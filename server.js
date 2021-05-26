3a17face272b7d073fdff2f7f2b37c30dfa201ad
Davsav <davidbyu6@gmail.com>
2021-05-26 09:39:06 -0600

    fixed error

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const express = require('express');
 const { notes } = require('./data/notes')
 
-const PORT = process.env.PORT || 3001
-const app = express();
+const PORT = process.env.PORT || 3001;
+const app = express(); 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -64,6 +64,10 @@ app.post('/api/notes', (req, res) => {
         res.json(note);
     }
 });
+
+// app.delete('/api/notes:id', function (req, res) {
+//     res.send(path.join(__));
+//   })
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
